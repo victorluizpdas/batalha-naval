bb102b8f523cbaebfa17b0a0e4dad9a431b0923c
Vikkktor <luizpereiradasilva@gmail.com>
2026-06-24 15:17:21 -0300

    finalizado

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <random>
 #include <map>
+#include <ctime>
+#include <windows.h>
 
 #include <termcolor/termcolor.hpp>
 
@@ -74,6 +76,232 @@ void putShip(char ship, vector<vector<char>>& YourTable){
     
 }
 
+void putEnemyShip(vector<vector<char>>& EnemyTable, map<char, int>& EnemyShips){
+
+    switch(rand() % 10){
+
+    case 0:
+
+        // 10 Corvetas
+        for(int i=0;i<3;i++) EnemyTable[1][1+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[3][6+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[5][11+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[7][16+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[10][2+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[12][7+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[14][12+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[16][1+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[18][8+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[18][15+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+
+    case 1:
+
+        for(int i=0;i<4;i++) EnemyTable[1+i][2]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[1+i][6]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[1+i][10]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[10+i][4]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[10+i][9]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[10+i][14]='F';
+       EnemyShips['F']+=4;
+
+        break;
+
+    case 2:
+
+        for(int i=0;i<5;i++) EnemyTable[2][2+i]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<5;i++) EnemyTable[8][8+i]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<5;i++) EnemyTable[15][12+i]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<3;i++) EnemyTable[4+i][15]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[9+i][2]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[12+i][5]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[5][10+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[18][6+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+
+    case 3:
+
+        for(int x=0;x<6;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[2+y][2+x]='M';
+       EnemyShips['M']+=12;
+
+        for(int x=0;x<6;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[12+y][10+x]='M';
+       EnemyShips['M']+=12;
+
+        for(int i=0;i<3;i++) EnemyTable[18][1+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[18][16+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+
+    case 4:
+
+        for(int x=0;x<8;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[5+y][5+x]='P';
+       EnemyShips['P']+=16;
+
+        for(int i=0;i<4;i++) EnemyTable[15][2+i]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[15][12+i]='F';
+       EnemyShips['F']+=4;
+
+        break;
+
+    case 5:
+
+        for(int x=0;x<3;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[1+y][1+x]='S';
+       EnemyShips['S']+=12;
+
+        for(int x=0;x<3;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[1+y][10+x]='S';
+       EnemyShips['S']+=12;
+
+        for(int x=0;x<3;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[8+y][5+x]='S';
+       EnemyShips['S']+=12;
+
+        for(int x=0;x<3;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[15+y][14+x]='S';
+       EnemyShips['S']+=12;
+
+        for(int x=0;x<3;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[12+y][1+x]='S';
+       EnemyShips['S']+=12;
+
+        break;
+
+    case 6:
+
+        for(int x=0;x<8;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[2+y][2+x]='P';
+       EnemyShips['P']+=16;
+
+        for(int x=0;x<6;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[12+y][12+x]='M';
+       EnemyShips['M']+=12;
+
+        break;
+
+    case 7:
+
+        for(int i=0;i<5;i++) EnemyTable[1][1+i]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<5;i++) EnemyTable[18][10+i]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<4;i++) EnemyTable[6+i][15]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[8][3+i]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<3;i++) EnemyTable[12][5+i]='C';
+        EnemyShips['C']+=3;
+
+        for(int i=0;i<3;i++) EnemyTable[4][10+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+
+    case 8:
+
+        for(int x=0;x<6;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[8+y][7+x]='M';
+       EnemyShips['M']+=12;
+
+        for(int i=0;i<4;i++) EnemyTable[5+i][5]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<4;i++) EnemyTable[5+i][14]='F';
+       EnemyShips['F']+=4;
+
+        for(int i=0;i<3;i++) EnemyTable[13][9+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+
+    case 9:
+
+        for(int x=0;x<8;x++)
+            for(int y=0;y<2;y++)
+                EnemyTable[1+y][10+x]='P';
+       EnemyShips['P']+=16;
+
+        for(int i=0;i<5;i++) EnemyTable[10+i][2]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<5;i++) EnemyTable[10+i][17]='D';
+       EnemyShips['D']+=5;
+
+        for(int i=0;i<3;i++) EnemyTable[18][7+i]='C';
+        EnemyShips['C']+=3;
+
+        break;
+    }
+}
 
 void pickShip(int& pontos, vector<vector<char>>& YourTable, map< char, int>& YourShips,  map<char, int>& tamanhos){
     map<char, int> custos = {{'C', 3} , {'F', 5}, {'D', 7}, {'M', 10}, {'P',15}, {'S',15}};
@@ -170,7 +398,6 @@ void showTable(vector<vector<char>>& EnemyTable,  vector<vector<char>>& YourTabl
 
         cout<<termcolor::on_grey<< termcolor::white<<endl;
     }
-
 }
 
 int sumships(map< char, int>& Ships){
@@ -182,7 +409,7 @@ int sumships(map< char, int>& Ships){
     return sum;
 }
 
-void jogada(vector<vector<char>>& EnemyTable, vector<vector<bool>>& EnemyTableHits, map< char, int>& EnemyShips, map<char, int>& tamanhos ){
+void jogada(vector<vector<char>>& EnemyTable, vector<vector<bool>>& EnemyTableHits, map< char, int>& EnemyShips){
     int coluna, linha, municao;
     char x;
     bool valido = false;
@@ -209,12 +436,16 @@ void jogada(vector<vector<char>>& EnemyTable, vector<vector<bool>>& EnemyTableHi
             }
         }
     }
+    if(EnemyTable[linha][coluna] == '~'){
+        cout<<termcolor::bright_red<<"\nERROU\n";
+        EnemyTable[linha][coluna] = true;
 
+    }else{
     if(municao == 1){
         if(EnemyTable[linha][coluna] != 'S'){
             EnemyTableHits[linha][coluna] = true;
             cout<<termcolor::green<<"\nACERTOU!\n";
-            EnemyShips[EnemyTable[linha][coluna]] -= tamanhos[EnemyTable[linha][coluna]];
+            EnemyShips[EnemyTable[linha][coluna]] --;
         }else{
             cout<<termcolor::bright_red<<"\nMUNICAO INCAPAZ DE ATINGIR SUBMARINOS!\n";
         }
@@ -222,19 +453,61 @@ void jogada(vector<vector<char>>& EnemyTable, vector<vector<bool>>& EnemyTableHi
         if(EnemyTable[linha][coluna] == 'S'){
             EnemyTableHits[linha][coluna] = true;
             cout<<termcolor::green<<"\nACERTOU!\n";
-            EnemyShips[EnemyTable[linha][coluna]] -= tamanhos[EnemyTable[linha][coluna]];
+            EnemyShips[EnemyTable[linha][coluna]] --;
         }else{
             cout<<termcolor::bright_red<<"\nMUNICAO SEM EFEITO EM EMBARCACOES COMUNS!\n";
         }
     }
+}
 
 }
 
+void jogadaEnemy(vector<vector<char>>& EnemyTable, vector<vector<bool>>& EnemyTableHits, map< char, int>& EnemyShips){
+
+    int coluna, linha, municao;
+    char x;
+    bool valido = false;
+
+     while(!valido){
+        coluna = rand() % 20;
+        linha = rand() % 20;
+        municao = rand() % 2;
+        municao ++;
+
+        if(!EnemyTableHits[linha][coluna]){
+            valido = true;
+        }
+    }
+            
+    if(EnemyTable[linha][coluna] == '~'){
+        cout<<termcolor::green<<"\nINIMIGOP ERROU\n";
+        EnemyTable[linha][coluna] = true;
+    }else{
+    if(municao == 1){
+        if(EnemyTable[linha][coluna] != 'S'){
+            EnemyTableHits[linha][coluna] = true;
+            cout<<termcolor::red<<"\nINIMIGO ACERTOU!\n";
+            EnemyShips[EnemyTable[linha][coluna]] --;
+        }else{
+            cout<<termcolor::green<<"\nMUNICAO INIMIGA INCAPAZ DE ATINGIR SUBMARINOS!\n";
+        }
+    }else{
+        if(EnemyTable[linha][coluna] == 'S'){
+            EnemyTableHits[linha][coluna] = true;
+            cout<<termcolor::red<<"\nINIMIGO ACERTOU!\n";
+            EnemyShips[EnemyTable[linha][coluna]] --;
+        }else{
+            cout<<termcolor::green<<"\nMUNICAO INIMIGA SEM EFEITO EM EMBARCACOES COMUNS!\n";
+        }
+    }}
+}
+
 int main(){
+    srand(time(nullptr));
     vector<vector<char>> EnemyTable(20, vector<char>(20, '~')) , YourTable(20, vector<char>(20, '~'));
     vector<vector<bool>> EnemyTableHits(20, vector<bool>(20, 0)), YourTableHits(20, vector<bool>(20, 0));
     map<char, int> YourShips = {{'C', 0} , {'F', 0}, {'D', 0}, {'M', 0}, {'P',0}, {'S',0}} , EnemyShips = {{'C', 0} , {'F', 0}, {'D', 0}, {'M', 0}, {'P',0}, {'S', 0}},
-    tamanhos = {{'C', 3} , {'F', 4}, {'D', 5}, {'M', 12}, {'P',16}, {'S',12}};
+    tamanhos = {{'C', 3} , {'F', 4}, {'D', 5}, {'M', 12}, {'P',16}, {'S',6}};
     int pontos = 30;
 
 
@@ -254,13 +527,25 @@ int main(){
         cout<<"-";
     }
     cout<<termcolor::on_grey<<termcolor::white;
-
-   while(sumships(YourShips) && sumships(EnemyShips)){
-
-    jogada(EnemyTable, EnemyTableHits, EnemyShips, tamanhos);
+    putEnemyShip(EnemyTable, EnemyShips);
+    cout<<termcolor::on_grey<<termcolor::white;
 
 
+    while(sumships(YourShips) && sumships(EnemyShips)){
 
+    jogada(EnemyTable, EnemyTableHits, EnemyShips);
+    if(!sumships(EnemyShips)){
+        break;
+    }
+    jogadaEnemy(YourTable, YourTableHits, YourShips);
+
+    showTable( EnemyTable,   YourTable,   EnemyTableHits,  YourTableHits);
+    }
+
+    if(!sumships(EnemyShips)){
+        cout<<endl<<termcolor::on_bright_green<<termcolor::bright_white<<"VOCE GANHOU UM PREMIO DE 10 MIL MILHOES";
+    }else{
+         cout<<endl<<termcolor::on_bright_red<<termcolor::bright_white<<"VOCE PERDEU UM PREMIO DE 10 MIL MILHOES";
     }
 
 
